f0d9c63a97e593c3b9b03d099d8df3c402d594a4
Vladyslav Zaporozhets <ziper.step@gmail.com>
2025-10-26 11:18:52 +0100

    add

---
+++ src/task-6.ts
@@ -0,0 +1,7 @@
+function getFirstElement<T>(arr: T[]): T | undefined {
+  return arr[0];
+}
+
+getFirstElement<number>([1, 2, 3]);
+getFirstElement<string>(["a", "b", "c"]);
+getFirstElement<boolean>([true, false, true]);
